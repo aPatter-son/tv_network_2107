d0844b7e3edfc415e0b1bfca9682410012e27713
Andrew Patterson <andrew.patterson747@gmail.com>
2021-07-21 11:56:14 -0500

    Initialize arguments

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -1,4 +1,5 @@
 require './lib/character'
+require 'pry'
 
 RSpec.describe Character do
   it 'exists' do
@@ -7,19 +8,19 @@ RSpec.describe Character do
     expect(character).to be_a(Character)
   end
 
-  it '' do
+  it 'calls the name' do
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
 
     expect(character.kitt.name).to eq("KITT")
   end
 
-  xit '' do
+  xit 'calls the actor' do
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
 
     expect(character.kitt.actor).to eq("William Daniels")
   end
 
-  xit '' do
+  xit 'calls the salary' do
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
 
     expect(character.kitt.salary).to eq(1000000)
